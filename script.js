bf5802b1fc6759422ca313d84167972755efadf2
coldgreen101 <109731064+coldgreen101@users.noreply.github.com>
2026-09-25 17:18:41 +0100

    Write pseudocode for getComputerChoice and getHumanChoice functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello, World!');
+/* 
+Create a function named 'getComputerChoice'
+
+  RETURN 'rock', 'paper' or 'scissors' at random
+
+  IF random number is between 0 and 0.33 
+    RETURN 'rock'
+
+  ELSE IF random number is between 0.33 and 0.67 
+    RETURN 'paper'
+
+  ELSE 
+    RETURN 'scissors'
+
+  Test output in the console
+
+Create a function named 'getHumanChoice'
+
+  Return user's choice/input
+
+  Test output in console
+
+NOTE: Assumme user will always enter a valid choice
+*/
